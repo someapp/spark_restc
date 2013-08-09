b518aac51ab85b4d23c4ec146fe9f79c9ff26986
root <root@chat.dev0.spark.net>
2013-08-09 13:17:55 -0400

    reading from app env variable

--- src/spark_restc_app.erl
+++ src/spark_restc_app.erl
@@ -5,15 +5,45 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(APP, spark_restc_app).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Conf_path = a, <<"etc">>},
-  	Conf_file, <<spark_restc.yaml>>},
-  	Use_mnesia_conf_store, false}
-    spark_restc_sup:start_link().
+	ok = ensure_dependency_started(),
+    Conf_path = 
+    		application:get_env(?APP, conf_path,undefined),
+  	Conf_file = 
+  			application:get_env(?APP, conf_file,undefined),
+  	Use_mnesia_conf_store = 
+  			application:get_env(?APP, use_mnesia_conf_store, false),
+	error_logger:info_msg("[~p] Using config path ~p",
+			[?APP, Conf_path]),
+	error_logger:info_msg("[~p] Using config file: ~p",
+			[?APP, Conf_file]),
+	error_logger:info_msg("[~p] Using mnesia as config store? ~p",
+			[?APP, Use_mnesia_conf_store]),
+    spark_restc_sup:start_link(
+    		[Conf_path, Conf_file, Use_mnesia_conf_store]).
 
 stop(_State) ->
     ok.
+
+ensure_dependency_started()->
+  ?INFO_MSG("[~p] Starting depedenecies", [?APP]),
+  Apps = [syntax_tools, 
+		compiler, 
+		crypto,
+		public_key,
+		ssl, 
+		goldrush,
+		lager, 
+		inets, 
+		restc],
+  error_logger("[~p] Going to start apps ~p",
+  	  [?APP, lists:flatten(Apps)]),
+  app_util:start_apps(Apps),
+  error_logger("[~p] Started depedenecies ~p",
+      [?APP, lists:flatten(Apps)]).
