9c3fdcf3b21a31d850034e00181024f6ceffcf43
root <root@chat.dev0.spark.net>
2013-08-10 13:05:58 -0400

    skeletin gen_server code

--- src/spark_restc_gen_rest_server.erl
+++ src/spark_restc_gen_rest_server.erl
@@ -24,6 +24,7 @@
 -type reason() :: tuple().
 -type ok_or_error():: {ok, state() | {error, reason()}.
 
+-define(SERVER, ?MODULE).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -35,12 +36,56 @@
 
 	
 get_active_handlers()->
-
+  gen_server:call(?SERVER, {get_active_handlers}).
 
 start_handler(Name) when is_atom(Name)->
+  gen_server:call(?SERVER, {start_handler, Name}).
+
+kill_handler(Name) when is_atom(Name)->
+  gen_server:call(?SERVER, {kill_handler, Name}).
+
+sniff_handler(Name, Level) when is_atom(Name)->
+  gen_server:call(?SERVER, {sniff_handler, Name, Level}).
+
+handle_call({get_active_handlers}, From, State)->
+
+  {ok, Reply, State};
+  
+handle_call({start_handler, Name}, From, State)->
+
+  {ok, Reply, State};
+
+handle_call({kill_handler, Name}, From, State)->
+
+  {ok, Reply, State};
+
+handle_call({sniff_handler, Name, Level}, From, State)->
+
+  {ok, Reply, State};
 
 
--ifdef(TEST)
+handle_call(Request, From, State)->
+
+  {ok, Reply, State}.
+
+handle_cast(Request, State)->
+  {noreply, State}
+
+handle_info(Request, State)->
+
+  {noreply, State}.
+
+termiante(stop, Why)->
+  {ok, Why};
+  
+terminate(Msg, Why)->
+  {ok, Why}.
+ 
+code_change(OldVsn, State, _Extra)->
+  NewState = State,  
+  {ok, NewState}.
+
+-ifdef(TEST).
 
 
 
